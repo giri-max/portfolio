b17c789873a242d6ce52df4c43157eec8ff337d0
giri-max <girivirat2718@gmail.com>
2024-09-16 11:44:54 +0530

    year update

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+//update cuurent year in copyright.
+
+const current_year=new Date().getFullYear();
+document.getElementById("year").innerText=current_year
